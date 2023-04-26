0ffd102116e41c0418c5ab9b76d06e954c9afc80
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-04-25 21:42:52 -0400

    created basis for server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const db = require('./config/connection');
+const path = require('path')
+//const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
+
+//app.use(routes);
+
+db.sync({force: false}).then(() => {
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}`);
+    })
+})
