431a1dd27002f8777395695506fd9b63223cdc20
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-04-27 21:15:28 -0400

    added routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 const path = require('path')
-//const routes = require('./controllers');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
 
-//app.use(routes);
+app.use(routes);
 
 db.sync({force: false}).then(() => {
     app.listen(PORT, () => {
