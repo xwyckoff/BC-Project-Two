fef6134b819137e478ae766757542bba47a60137
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-05-01 19:13:52 -0400

    added routes for home page to render handlebars template

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const api = require('./api/index')
+const homeRoutes = require('./homeRoutes');
 
+router.use('/', homeRoutes);
 router.use('/api', api);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const db = require('./config/connection');
+const exphbs = require('express-handlebars');
 const path = require('path')
 const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
