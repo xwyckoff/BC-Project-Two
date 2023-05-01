dd4bf82f47a2b838668f5d99531c58a064d8d1d0
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-05-01 19:36:23 -0400

    added routes for the tabs on the navbar

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('home', {title: 'TechBuilds'});
+    res.render('home', {title: 'TechBuilds', home_active: true});
+})
+
+router.get('/products', (req, res) => {
+    res.render('products', {title: 'Products', products_active: true});
+})
+
+router.get('/login', (req, res) => {
+    res.render('login', {title: 'Login', login_active: true});
 })
 
 module.exports = router;
