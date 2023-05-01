0bb7bebb86f2e293efd6e38566824ea81cbe9e0e
Janiece-Lewis <jlewilewi@gmail.com>
2023-05-01 19:59:19 -0400

    added cart item model

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const api = require('./api/index')
-
+const api = require('./api/index');
+const homeRoutes = require('./homeRoutes');
+router.use('/',homeRoutes);
 router.use('/api', api);
 
 module.exports = router;
