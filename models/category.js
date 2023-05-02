070da9d8227eec4c6298454687d1a17efc340246
Evil Cathedral <madischneider3@gmail.com>
2023-05-01 21:29:03 -0400

    added models for products, category, and brand

---
+++ models/category.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    brand_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references:{
+        model:'products',
+        key:'id'
+      }
+    },
+    
+  
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "category",
+  }
+);
+
+module.exports = Category;
