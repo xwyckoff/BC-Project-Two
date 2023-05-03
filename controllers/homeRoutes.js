290397eae67224ffbb9c5aa8a0ad06c5b66443f5
Janiece-Lewis <jlewilewi@gmail.com>
2023-05-03 16:21:17 -0400

    updated with cart and product

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const {Cart,Product}=require('../models')
 router.get('/', (req, res) => {
     res.render('home', {title: 'TechBuilds', home_active: true, logged_in: req.session.logged_in, username: req.session.username});
 })
@@ -18,9 +18,21 @@ router.get('/login', (req, res) => {
 
 router.get('/cart', async (req, res) => {console.log('inside the cart')
     try {
-      
+      // Get all projects and JOIN with user data
+    const projectData = await Cart.findAll({
+      include: [
+        {
+          model: Product,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const products = productData.map((product) => product.get({ plain: true }));
+ 
       // Pass serialized data and session flag into template
-      res.render('cart');
+      res.render('cart',{products});
     } catch (err) {
       res.status(500).json(err);
     }
