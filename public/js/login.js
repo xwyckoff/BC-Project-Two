c05b7cf5cb59878529e1ab672f1d318f42089596
Clay_MM <clay.moyer.1515@gmail.com>
2023-05-04 21:00:08 -0400

    undid a redo

--- public/js/login.js
+++ public/js/login.js
@@ -3,8 +3,6 @@ const loginFormHandler = async (event) => {
 
     let username = document.querySelector('#login-username').value.trim();
     let password = document.querySelector('#login-password').value.trim();
-    console.log(username);
-    console.log(password);
 
     if(username && password) {
         const response = await fetch('/api/users/login', {
